1a5573031a96dde384d741106fe494c9957f3860
Aayush Rathod <aayushrathod521@gmail.com>
2025-06-20 20:15:18 -0400

    This is the commit for branch 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is the base code");
+        System.out.println("This is the code change for the new created branch");
     }
 }
